e3e31c310b57a9898ca1b911882def27752424b9
nikeshvpatel <ptlnik13@gmail.com>
2026-04-22 11:59:44 -0400

    Basic UI setup has done

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import {cn} from "@/lib/utils";
 import {Inter} from "next/font/google";
+import {Toaster} from "@/components/ui/sonner";
 
 const geistSans = localFont({
     src     : "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({children}) {
             className={cn(`${geistSans.variable} ${geistMono.variable} antialiased, ${poppins.className}`)}
         >
         {children}
+        <Toaster richColors position='top-center'/>
         </body>
         </html>
     );
--- components/Test.jsx
+++ components/Test.jsx
@@ -1,10 +1,18 @@
+'use client'
 import React from 'react';
 import {Button} from "./ui/button";
+import {toast} from "sonner";
 
 function Test(props) {
+
+    const handleClick = (mode) => {
+        mode ? toast.success("Success") : toast.error("Error");
+
+    }
+
     return (
         <div>
-            <Button>Click me</Button>
+            <Button onClick={() => handleClick(false)}>Click me</Button>
         </div>
     );
 }
