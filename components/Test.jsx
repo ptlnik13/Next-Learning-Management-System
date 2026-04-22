119c8506a7942ddd887c4f833661a1b31ecc7f6c
nikeshvpatel <ptlnik13@gmail.com>
2026-04-22 10:52:35 -0400

    Testing the Button component and updating imports

---
+++ components/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "./ui/button";
+
+function Test(props) {
+    return (
+        <div>
+            <Button>Click me</Button>
+        </div>
+    );
+}
+
+export default Test;
