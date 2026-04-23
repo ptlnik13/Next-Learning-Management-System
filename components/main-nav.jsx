1d83462b3abeddfbd4bc88dd05384baac417bcec
nikeshvpatel <ptlnik13@gmail.com>
2026-04-22 22:48:46 -0400

    Nav bar- Login section

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
 import Logo from './logo';
@@ -9,7 +10,7 @@ import {Menu} from 'lucide-react';
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from './ui/dropdown-menu';
 import {Avatar, AvatarFallback, AvatarImage} from './ui/avatar';
 
-const MainNav = ({items, header, children}) => {
+const MainNav = ({items, children}) => {
     return (
         <>
             <div className='flex gap-6 lg:gap-10'>
@@ -33,10 +34,61 @@ const MainNav = ({items, header, children}) => {
                         </nav>
                     ) : null
                 }
+
             </div>
-            <nav>
-                login
+
+            <nav className='flex items-center gap-3'>
+                <div className='items-center gap-3 hidden lg:flex'>
+                    <Link href='/login' className={cn(buttonVariants({size: "sm"}), "px-4")}>
+                        Login
+                    </Link>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="sm">Register</Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="w-56 mt-4">
+                            <DropdownMenuItem className="cursor-pointer">
+                                <Link href=''>Student</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className="cursor-pointer">
+                                <Link href=''>Instructor</Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <div className='cursor-pointer'>
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" alt="@ariyan"/>
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </div>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent align="end" className="w-56 mt-4">
+                        <DropdownMenuItem className="cursor-pointer" asChild>
+                            <Link href='account'>Profile</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer" asChild>
+                            <Link href='account/enrolled-courses'>My Courses</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer" asChild>
+                            <Link href=''>Testimonials & Certificates</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer" asChild>
+                            <Link href=''>Logout</Link>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+
+
+                </DropdownMenu>
+
+
             </nav>
+
         </>
 
 
