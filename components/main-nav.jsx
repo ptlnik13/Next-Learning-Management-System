22d7c7c4f0d08a423371fe13a3f5a0055aebbb17
nikeshvpatel <ptlnik13@gmail.com>
2026-04-22 22:53:50 -0400

    Mobile Nav scaffolding

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link';
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from './logo';
 import {cn} from '@/lib/utils';
 
@@ -9,8 +9,11 @@ import {Button, buttonVariants} from './ui/button';
 import {Menu} from 'lucide-react';
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from './ui/dropdown-menu';
 import {Avatar, AvatarFallback, AvatarImage} from './ui/avatar';
+import MobileNav from "./mobile-nav";
 
 const MainNav = ({items, children}) => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
     return (
         <>
             <div className='flex gap-6 lg:gap-10'>
@@ -34,7 +37,9 @@ const MainNav = ({items, children}) => {
                         </nav>
                     ) : null
                 }
-
+                {showMobileMenu && items && (
+                    <MobileNav items={items}>{children}</MobileNav>
+                ) }
             </div>
 
             <nav className='flex items-center gap-3'>
@@ -85,7 +90,9 @@ const MainNav = ({items, children}) => {
 
 
                 </DropdownMenu>
-
+                <button className='flex items-center space-x-2 lg:hidden' onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                    {showMobileMenu ? <X/> : <Menu/> }
+                </button>
 
             </nav>
 
---
+++ components/mobile-nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+
+import { X } from 'lucide-react';
+import { Button, buttonVariants } from './ui/button';
+import { Menu } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+
+const MobileNav = ({items,children}) => {
+    return (
+        <div>
+            mobile
+        </div>
+    );
+};
+
+export default MobileNav;
