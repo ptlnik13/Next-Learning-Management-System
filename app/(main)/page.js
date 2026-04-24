dc4d369440e22aa9c7e2e9baec00e1316ea1840d
nikeshvpatel <ptlnik13@gmail.com>
2026-04-23 21:44:14 -0400

    Fetch categories dynamically and update category links

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -11,53 +11,55 @@ import Image from "next/image";
 import Link from "next/link";
 import {getCourseList} from "../../queries/courses";
 import CourseCard from "./courses/_components/CourseCard";
+import {getCategories} from "../../queries/categories";
 
-const categories = [
-    {
-        id       : 1,
-        title    : "Design",
-        thumbnail: "/assets/images/categories/design.jpg",
-    },
-
-    {
-        id       : 3,
-        title    : "Development",
-        thumbnail: "/assets/images/categories/development.jpg",
-    },
-    {
-        id       : 4,
-        title    : "Marketing",
-        thumbnail: "/assets/images/categories/marketing.jpg",
-    },
-    {
-        id       : 5,
-        title    : "IT & Software",
-        thumbnail: "/assets/images/categories/it_software.jpg",
-    },
-    {
-        id       : 6,
-        title    : "Personal Development",
-        thumbnail: "/assets/images/categories/personal_development.jpg",
-    },
-    {
-        id       : 7,
-        title    : "Business",
-        thumbnail: "/assets/images/categories/programming.jpg",
-    },
-    {
-        id       : 8,
-        title    : "Photography",
-        thumbnail: "/assets/images/categories/photography.jpg",
-    },
-    {
-        id       : 9,
-        title    : "Music",
-        thumbnail: "/assets/images/categories/music.jpg",
-    },
-];
+// const categories = [
+//     {
+//         id       : 1,
+//         title    : "Design",
+//         thumbnail: "/assets/images/categories/design.jpg",
+//     },
+//
+//     {
+//         id       : 3,
+//         title    : "Development",
+//         thumbnail: "/assets/images/categories/development.jpg",
+//     },
+//     {
+//         id       : 4,
+//         title    : "Marketing",
+//         thumbnail: "/assets/images/categories/marketing.jpg",
+//     },
+//     {
+//         id       : 5,
+//         title    : "IT & Software",
+//         thumbnail: "/assets/images/categories/it_software.jpg",
+//     },
+//     {
+//         id       : 6,
+//         title    : "Personal Development",
+//         thumbnail: "/assets/images/categories/personal_development.jpg",
+//     },
+//     {
+//         id       : 7,
+//         title    : "Business",
+//         thumbnail: "/assets/images/categories/programming.jpg",
+//     },
+//     {
+//         id       : 8,
+//         title    : "Photography",
+//         thumbnail: "/assets/images/categories/photography.jpg",
+//     },
+//     {
+//         id       : 9,
+//         title    : "Music",
+//         thumbnail: "/assets/images/categories/music.jpg",
+//     },
+// ];
 
 const HomePage = async () => {
     const courses = await getCourseList();
+    const categories = await getCategories();
 
     return (
         <>
@@ -118,13 +120,13 @@ const HomePage = async () => {
                     {categories.map((category) => {
                         return (
                             <Link
-                                href=""
+                                href={`/categories/${category.id}`}
                                 key={category.id}
                                 className="relative overflow-hidden rounded-lg border bg-background p-2 hover:scale-105 transition-all duration-500 ease-in-out"
                             >
                                 <div className="flex  flex-col gap-4 items-center justify-between rounded-md p-6">
                                     <Image
-                                        src={category.thumbnail}
+                                        src={`/assets/images/categories/${category.thumbnail}`}
                                         alt={category.title}
                                         width={100}
                                         height={100}
