0005c1a56c840f84400e1819ecd625c675512f3e
nikeshvpatel <ptlnik13@gmail.com>
2026-04-26 12:59:27 -0400

    Fetch course details dynamically in SingleCoursePage and add `getCourseDetails` query method

--- queries/courses.js
+++ queries/courses.js
@@ -3,7 +3,7 @@ import {Course} from "@/model/course-model";
 import {User} from "@/model/user-model";
 import {Testimonial} from "@/model/testimonial-model";
 import {Module} from "@/model/module-model";
-import {replaceMongoIdInArray} from "@/lib/convertData";
+import {replaceMongoIdInArray, replaceMongoIdInObject} from "@/lib/convertData";
 
 export async function getCourseList() {
     const courses = await Course.find({}).select(['title', 'subtitle', 'thumbnail', 'modules', 'price', 'category', 'instructor']).populate({
@@ -21,4 +21,27 @@ export async function getCourseList() {
     }).lean();
 
     return replaceMongoIdInArray(courses);
+}
+
+
+export async function getCourseDetails(id) {
+    const course = await Course.findById(id)
+        .populate({
+            path : "category",
+            model: Category
+        }).populate({
+            path : "instructor",
+            model: User
+        }).populate({
+            path    : "testimonials",
+            model   : Testimonial,
+            populate: {
+                path : "user",
+                model: User
+            }
+        }).populate({
+            path : "modules",
+            model: Module
+        }).lean();
+    return replaceMongoIdInObject(course);
 }
