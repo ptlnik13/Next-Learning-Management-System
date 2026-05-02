7d450ac06d8a6ab82ab6c391b6d5a97c921a644e
nikeshvpatel <ptlnik13@gmail.com>
2026-05-02 16:39:31 -0400

    Integrate session handling with `next-auth`: wrap layout in `SessionProvider`, enhance `MainNav` to display dynamic login/logout menu based on session state.

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SiteFooter from '@/components/site-footer';
 import MainNav from "../../components/main-nav";
+import {SessionProvider} from "next-auth/react";
 
 const navLinks = [
     {
@@ -26,10 +27,11 @@ function MainLayout({children}) {
     return (
         <div className='flex min-h-screen flex-col'>
             <header className='z-40 bg-background/60 backdrop-blur-md fixed top-0 left-0 right-0 border-b'>
-                <div className='container flex h-20 items-center justify-between py-6'>
-                    <MainNav items={navLinks}/>
-                </div>
-
+                <SessionProvider>
+                    <div className='container flex h-20 items-center justify-between py-6'>
+                        <MainNav items={navLinks}/>
+                    </div>
+                </SessionProvider>
             </header>
 
             <main className='flex-1 pt-20 flex flex-col border-b border-gray-700'> {children} </main>
--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Logo from './logo';
 import {cn} from '@/lib/utils';
 
@@ -10,10 +10,16 @@ import {Menu} from 'lucide-react';
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from './ui/dropdown-menu';
 import {Avatar, AvatarFallback, AvatarImage} from './ui/avatar';
 import MobileNav from "./mobile-nav";
+import {useSession} from "next-auth/react";
 
 const MainNav = ({items, children}) => {
+    const {data: session} = useSession();
     const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [loginSession, setLoginSession] = useState(null);
 
+    useEffect(() => {
+        setLoginSession(session);
+    }, [session])
     return (
         <>
             <div className='flex gap-6 lg:gap-10'>
@@ -43,53 +49,59 @@ const MainNav = ({items, children}) => {
             </div>
 
             <nav className='flex items-center gap-3'>
-                <div className='items-center gap-3 hidden lg:flex'>
-                    <Link href='/login' className={cn(buttonVariants({size: "sm"}), "px-4")}>
-                        Login
-                    </Link>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="sm">Register</Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end" className="w-56 mt-4">
-                            <Link href='/register/student'>
-                                <DropdownMenuItem className="cursor-pointer">Student</DropdownMenuItem>
+                {
+                    !loginSession && (
+                        <div className='items-center gap-3 hidden lg:flex'>
+                            <Link href='/login' className={cn(buttonVariants({size: "sm"}), "px-4")}>
+                                Login
                             </Link>
-                            <Link href='/register/instructor'>
-                                <DropdownMenuItem className="cursor-pointer">Instructor</DropdownMenuItem>
-                            </Link>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-
-
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <div className='cursor-pointer'>
-                            <Avatar>
-                                <AvatarImage src="https://github.com/shadcn.png" alt="@ariyan"/>
-                                <AvatarFallback>CN</AvatarFallback>
-                            </Avatar>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant="outline" size="sm">Register</Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end" className="w-56 mt-4">
+                                    <Link href='/register/student'>
+                                        <DropdownMenuItem className="cursor-pointer">Student</DropdownMenuItem>
+                                    </Link>
+                                    <Link href='/register/instructor'>
+                                        <DropdownMenuItem className="cursor-pointer">Instructor</DropdownMenuItem>
+                                    </Link>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
-                    </DropdownMenuTrigger>
+                    )
+                }
+                {
+                    loginSession && (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <div className='cursor-pointer'>
+                                    <Avatar>
+                                        <AvatarImage src="https://github.com/shadcn.png" alt="@ariyan"/>
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                </div>
+                            </DropdownMenuTrigger>
 
-                    <DropdownMenuContent align="end" className="w-56 mt-4">
-                        <DropdownMenuItem className="cursor-pointer" asChild>
-                            <Link href='account'>Profile</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer" asChild>
-                            <Link href='account/enrolled-courses'>My Courses</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer" asChild>
-                            <Link href=''>Testimonials & Certificates</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem className="cursor-pointer" asChild>
-                            <Link href=''>Logout</Link>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
+                            <DropdownMenuContent align="end" className="w-56 mt-4">
+                                <DropdownMenuItem className="cursor-pointer" asChild>
+                                    <Link href='account'>Profile</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="cursor-pointer" asChild>
+                                    <Link href='account/enrolled-courses'>My Courses</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="cursor-pointer" asChild>
+                                    <Link href=''>Testimonials & Certificates</Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="cursor-pointer" asChild>
+                                    <Link href=''>Logout</Link>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
 
 
-                </DropdownMenu>
+                        </DropdownMenu>
+                    )
+                }
                 <button className='flex items-center space-x-2 lg:hidden'
                         onClick={() => setShowMobileMenu(!showMobileMenu)}>
                     {showMobileMenu ? <X/> : <Menu/>}
